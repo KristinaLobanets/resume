38afa8ea0777e863fa304cd53bb0163b6387cacf
Kristina Lobanets <kristina.malitowska@gmail.com>
2020-10-14 19:46:54 +0300

    first

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,6 +6,7 @@ const Home = () => {
     <div classNames={styles.home}>
       <div classNames={styles.homephoto}></div>
       <h1 classNames={styles.homename}>Kristina Lobanets</h1>
+      <div classNames={styles.homeinfo}></div>
     </div>
   );
 };
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,169 @@ const Projects = () => {
   return (
     <div classNames={styles.home}>
       <h1 classNames={styles.hometitle}>Projects</h1>
+      <div classNames={styles.projectsBlock}>
+        <h2 classNames={styles.projectsTitle}>HTML/CSS</h2>
+        <ul classNames={styles.projectsUl}>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a
+              href="http://extreme-exterminating.com"
+              classNames={styles.projectsPageA}
+            >
+              Page
+            </a>
+            <p classNames={styles.projectsTxt}></p>
+          </li>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a
+              href="https://kristinalobanets.github.io/Lobanets_Test/Glyanec/"
+              classNames={styles.projectsPageA}
+            >
+              Page
+            </a>
+            <p classNames={styles.projectsTxt}></p>
+          </li>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a
+              href="https://kristinalobanets.github.io/goit-fe-course/html-css/module-10/"
+              classNames={styles.projectsPageA}
+            >
+              Page
+            </a>
+            <p classNames={styles.projectsTxt}></p>
+          </li>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a
+              href="https://kristinalobanets.github.io/goit-fe-course/html-css/module-07/"
+              classNames={styles.projectsPageA}
+            >
+              Page
+            </a>
+            <p classNames={styles.projectsTxt}></p>
+          </li>
+
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a
+              href="https://kristinalobanets.github.io/goit-fe-course/html-css/module-06/"
+              classNames={styles.projectsPageA}
+            >
+              Page
+            </a>
+            <p classNames={styles.projectsTxt}></p>
+          </li>
+
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a
+              href="https://kristinalobanets.github.io/goit-fe-course/html-css/module-05/"
+              classNames={styles.projectsPageA}
+            >
+              Page
+            </a>
+            <p classNames={styles.projectsTxt}></p>
+          </li>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a
+              href="https://kristinalobanets.github.io/goit-fe-course/html-css/module-04/"
+              classNames={styles.projectsPageA}
+            >
+              Page
+            </a>
+            <p classNames={styles.projectsTxt}></p>
+          </li>
+        </ul>
+      </div>
+      <div classNames={styles.projectsBlock}>
+        <h2 classNames={styles.projectsTitle}>JavaScript </h2>
+        <ul classNames={styles.projectsUl}>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a classNames={styles.projectsPageA}></a>
+          </li>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a classNames={styles.projectsPageA}></a>
+          </li>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a classNames={styles.projectsPageA}></a>
+          </li>
+        </ul>
+      </div>
+      <div classNames={styles.projectsBlock}>
+        <h2 classNames={styles.projectsTitle}>React</h2>
+        <ul classNames={styles.projectsUl}>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a classNames={styles.projectsPageA}></a>
+          </li>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a classNames={styles.projectsPageA}></a>
+          </li>
+          <li classNames={styles.projectsPage}>
+            <img
+              src="./image/"
+              alt="Previous "
+              classNames={styles.projectsPageImg}
+            />
+            <a classNames={styles.projectsPageA}></a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
